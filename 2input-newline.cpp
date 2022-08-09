a3ccc6fafae84606374514f745c22b3cb6705d34
Chanjnint Lim <100104333+Chanjnint21@users.noreply.github.com>
2022-08-09 08:24:18 +0700

    Create 2input-newline.cpp

---
+++ 2input-newline.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x, y;
+  cout << "Enter number with space seperate:"; // ask user to type input Example: 3 4
+  cin >> x  >> y; //take input from user to store in x
+  cout << x << endl << y; // print  x and y with newline
+  return 0;
+}
