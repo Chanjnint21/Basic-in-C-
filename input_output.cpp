f134073b2893c40a8c1a51ad83f2798dda01bc9a
Chanjnint Lim <100104333+Chanjnint21@users.noreply.github.com>
2022-08-09 08:15:14 +0700

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+  cout << "Enter your fav number:"; // ask user to type input
+  cin >> x; //take input from user to store in x
+  cout << x; //print x value
+    return 0;
+}
